505c553af0fbe5ad5f4383dbc2fd0822b1e33126
Agni <agni@matelabs.in>
2022-02-10 18:24:24 +0530

    Added newDeck function

--- deck.go
+++ deck.go
@@ -6,6 +6,22 @@ import "fmt"
 
 type deck []string 
 
+func newDeck() deck {
+
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardRank := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _,suit := range cardSuits {
+		for _,rank := range cardRank {
+			cards = append(cards, rank+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (dk deck) print() {
 	for i, card := range dk {
 		fmt.Println(i, card)
